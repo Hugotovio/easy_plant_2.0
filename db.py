f1329be456083e283ea6aa97b042bb3b4ca1f7eb
Tovy7 <gohutb@hotmail.com>
2026-02-05 22:37:37 -0500

    Reapply "ajustes de entorno"
    This reverts commit 0d09e060f288add1415a20f05671acb9d7da127b.

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 import os
 import mysql.connector
 
-# 🔹 SOLO carga .env si estás en LOCAL
+# SOLO usar dotenv en local
 if os.getenv("RAILWAY_ENVIRONMENT") is None:
     try:
         from dotenv import load_dotenv
@@ -12,31 +12,9 @@ if os.getenv("RAILWAY_ENVIRONMENT") is None:
 def get_connection():
     return mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
-        port=int(os.getenv("MYSQLPORT", 3306)),
+        port=int(os.getenv("MYSQLPORT")),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
         autocommit=True
     )
-
-def crear_tabla_liquidaciones_oca():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS liquidaciones_oca (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            tanque VARCHAR(50),
-            api FLOAT,
-            temperatura FLOAT,
-            volumen_recibido FLOAT,
-            volumen_calculado FLOAT,
-            tolerancia FLOAT,
-            diferencia FLOAT,
-            resultado VARCHAR(50),
-            fecha DATETIME
-        )
-    """)
-
-    cursor.close()
-    conn.close()
